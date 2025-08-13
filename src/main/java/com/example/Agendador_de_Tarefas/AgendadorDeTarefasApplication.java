a0c7aa12bb05f5c018f2f41780352bcc4900d726
Lucas Teixeira <lucast.leopoldino.rodrigues@gmail.com>
2025-08-12 23:38:59 -0300

    commit inicial com esqueleto

---
+++ src/main/java/com/example/Agendador_de_Tarefas/AgendadorDeTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.example.Agendador_de_Tarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendadorDeTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendadorDeTarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Agendador_de_Tarefas/AgendadorDeTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Agendador_de_Tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorDeTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
